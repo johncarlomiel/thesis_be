c9d263699b4c661e7124dd656fa94172a5f985ca
carlojohn98 <johncarlomiel@gmail.com>
2018-12-03 16:17:41 +0800

    Latest

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get("/api/checkSession", verifyToken, (req, res) => {
 
 app.get('/api/getInfo', verifyToken, (req, res) => {
     pool.getConnection((err, connection) => {
-        let tablenames = "name, course, age, gender, religion, place_birth, addr, cp_num, mother_name, mother_religion, mother_job, father_name, father_religion, father_job, not_livingwith_parents, study_status, transpo, allowed_night, study_helper, hobby, have_friends";
+        let tablenames = "name, year, course, age, gender, religion, place_birth, addr, cp_num, mother_name, mother_religion, mother_job, father_name, father_religion, father_job, not_livingwith_parents, study_status, transpo, allowed_night, study_helper, hobby, have_friends";
         if (err) throw err;
         let query = connection.query(`SELECT ${tablenames} FROM users WHERE id=${req.userData.id}`, (err, results, fields) => {
             if (err) throw err;
@@ -148,12 +148,12 @@ app.get('/api/getMoreInfo', verifyToken, (req, res) => {
 app.get('/api/getProblems', verifyToken, (req, res) => {
     pool.getConnection((err, connection) => {
         if (err) throw err;
-        let tablenames = "adjustments, courtship, education, family, financial, morals, per_physio, physio, soc_physio, soc_recreat, teach_procedure";
 
 
-        let query = connection.query(`SELECT ${tablenames} FROM problems WHERE user_id = ${req.userData.id}`, (error, results, fields) => {
+
+        let query = connection.query(`SELECT * FROM user_problems INNER JOIN problems ON user_problems.problem_id = problems.id WHERE user_id = ${req.userData.id} AND answer=1`, (error, results, fields) => {
             if (error) throw error;
-            res.status(200).json(results[0])
+            res.status(200).json(results)
         });
         console.log(query.sql)
 
@@ -166,7 +166,7 @@ app.put('/api/updateInfo', verifyToken, (req, res) => {
         if (err) throw err;
         let data = Object.values(req.body);
         let sql = `UPDATE users SET name = ?, course = ?, age = ?, gender = ?, religion = ?, place_birth = ?, addr = ?, cp_num = ?, mother_name = ?, mother_religion = ?, mother_job = ?,
-        father_name = ?, father_religion = ?, father_job = ?, not_livingwith_parents = ?, study_status = ?, transpo = ?, allowed_night = ?, study_helper = ?, hobby = ?, have_friends = ? WHERE id = ?`;
+        father_name = ?, father_religion = ?, father_job = ?, not_livingwith_parents = ?, study_status = ?, transpo = ?, allowed_night = ?, study_helper = ?, hobby = ?, have_friends = ?, year = ? WHERE id = ?`;
 
         data.push(req.userData.id)
         let qwe = connection.query(sql, data, (err, results, fields) => {
@@ -177,6 +177,7 @@ app.put('/api/updateInfo', verifyToken, (req, res) => {
 
             res.status(200).json({ message: "Updated" });
         });
+        console.log(qwe.sql)
 
 
     })
@@ -206,27 +207,54 @@ app.put('/api/updateMoreInfo', verifyToken, (req, res) => {
 
 app.put('/api/updateProblems', verifyToken, (req, res) => {
 
-    pool.getConnection((err, connection) => {
-        if (err) throw err;
-        let holder = req.body.data;
-        let data = Array.apply(null, Array());
-        holder.forEach(element => {
-            data.push(element.value);
+    let holder = Array.apply(null, Array());
+    req.body.data.forEach(element => {
+        element.value.forEach(element => {
+            holder.push(element)
         });
-        let sql = `UPDATE problems SET physio = ?, financial = ?, soc_recreat = ?, courtship = ?, soc_physio = ?, per_physio = ?, morals = ?, teach_procedure = ?, family = ?, education = ?, adjustments = ? WHERE user_id = ?`;
-        data.push(req.userData.id)
-        let query = connection.query(sql, data, (err, results, fields) => {
-            if (err) {
-                res.status(400).json({ err })
-                throw err;
-            }
-            res.status(200).json({ message: "updated" });
-        })
-        console.log(query.sql)
     });
 
+    pool.getConnection((err, connection) => {
+
+        holder.forEach(element => {
+            let query = connection.query(`SELECT * FROM user_problems WHERE user_id=${req.userData.id} AND problem_id=${element.id}`, (error, results, fields) => {
+                if (error) throw error;
+                if (results.length == 0) {
+                    let tanga = connection.query("INSERT INTO user_problems SET ?", { user_id: req.userData.id, problem_id: element.id, answer: element.value }, (error, results, fields) => {
+                        if (error) throw error;
+                    });
+
+
+                } else {
+                    connection.query("UPDATE user_problems SET answer = ? WHERE user_id = ? AND problem_id = ?", [element.value, req.userData.id, element.id], (error, results, fields) => {
+                        if (error) throw error;
+
+                    });
+                }
+
+            });
+
+
+        });
+
+
+        res.status(200).json({ status: 200, message: "Success!" })
+    });
 
 });
+
+app.get("/api/getQuestions", (req, res) => {
+    pool.getConnection((err, connection) => {
+
+        let query = connection.query("SELECT * FROM problems", (error, results, fields) => {
+            if (error) throw error;
+            res.status(200).json(results);
+
+        })
+
+    })
+});
+
 
 
 
@@ -259,10 +287,10 @@ function verifyToken(req, res, next) {
 
 app.get('/api/completeTest', verifyToken, (req, res) => {
     pool.getConnection((err, connection) => {
-        if(err) throw err;
+        if (err) throw err;
         let query = connection.query(`SELECT code FROM user_code WHERE user_id=${req.userData.id}`, (error, results, fields) => {
-            if(error) throw error;
-            
+            if (error) throw error;
+
         })
     })
 })
@@ -318,6 +346,154 @@ app.get('/api/getMyCode', verifyToken, (req, res) => {
 
 });
 
+
+//Admin Routes
+//Get all users
+app.get('/api/admin/users', (req, res) => {
+    pool.getConnection((err, connection) => {
+        connection.query("SELECT id, username, name FROM users", (error, results, fields) => {
+            res.status(200).json(results)
+        });
+    })
+})
+
+//Serach
+app.get("/api/admin/searchUsers", (req, res) => {
+    pool.getConnection((err, connection) => {
+        let query = connection.query(`SELECT * FROM users WHERE name LIKE '%${req.query.keyword}%'`, (error, results, fields) => {
+            if (error) throw error;
+            res.status(200).json(results)
+        })
+        console.log(query.sql)
+    })
+})
+
+app.get('/api/admin/getInfo', (req, res) => {
+    pool.getConnection((err, connection) => {
+
+        let tablenames = "name, course, age, gender, religion, place_birth, addr, cp_num, mother_name, mother_religion, mother_job, father_name, father_religion, father_job, not_livingwith_parents, study_status, transpo, allowed_night, study_helper, hobby, have_friends";
+        if (err) throw err;
+        let query = connection.query(`SELECT ${tablenames} FROM users WHERE id=${req.query.id}`, (err, results, fields) => {
+            if (err) throw err;
+            res.status(200).json(results[0]);
+
+        });
+
+    })
+});
+
+app.get('/api/admin/getMoreInfo', (req, res) => {
+    pool.getConnection((err, connection) => {
+        let tablenames = "troubling_problems, someone_to_talk, happiest_expi, downful_expi, ambition, want_to_change";
+        if (err) throw err;
+        let query = connection.query(`SELECT ${tablenames} FROM users WHERE id=${req.query.id}`, (err, results, fields) => {
+            if (err) throw err;
+            res.status(200).json(results[0]);
+
+        });
+    })
+});
+
+app.get('/api/admin/getProblems', (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+
+
+
+        let query = connection.query(`SELECT * FROM user_problems INNER JOIN problems ON user_problems.problem_id = problems.id WHERE user_id = ${req.query.id} AND answer=1`, (error, results, fields) => {
+            if (error) throw error;
+            res.status(200).json(results)
+        });
+        console.log(query.sql)
+
+    })
+});
+
+
+app.post("/api/admin/graph", (req, res) => {
+    console.log(req.body)
+    let result = {
+        conditionMet: "",
+        notCondition: "",
+        allResult: ""
+    }
+
+    let criteria_holder = "";
+    let criteriaMet = "";
+    let notCriteriaMet = "";
+
+    //Get result 1
+    //Get Criteria
+    req.body.criteria.forEach((first, firstIndex) => {
+        notCriteriaMet += "(";
+        criteriaMet += "(";
+        first.value.forEach((element, index) => {
+            notCriteriaMet += "users." + first.fieldname + " = " + `'${element}'`;
+            criteriaMet += "users." + first.fieldname + " = " + `'${element}'`;
+            if (index != first.value.length - 1) {
+                notCriteriaMet += " OR "
+                criteriaMet += " OR "
+            }
+        });
+        notCriteriaMet += ")";
+        notCriteriaMet += " AND "
+        criteriaMet += ")"
+        criteriaMet += " AND "
+
+    });
+
+    req.body.value.forEach((element, index) => {
+        notCriteriaMet += element + " = 0";
+        criteriaMet += element + " = 1";
+        if (index != req.body.value.length - 1) {
+            criteriaMet += " AND "
+            notCriteriaMet += " AND "
+        }
+
+    });
+
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        let query = connection.query(`SELECT * FROM users INNER JOIN reserve ON users.id = reserve.user_id WHERE ${criteriaMet}`, (error, results, fields) => {
+
+            if (error) throw error;
+
+        })
+
+
+    })
+    //Get result 2
+
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        let query = connection.query(`SELECT * FROM users INNER JOIN reserve ON users.id = reserve.user_id WHERE ${notCriteriaMet}`, (error, results, fields) => {
+
+            if (error) throw error;
+            console.log(results)
+
+        })
+        console.log(query.sql)
+
+    })
+
+
+
+
+
+
+});
+
+app.delete("/api/admin/users", (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+
+        let query = connection.query(`DELETE FROM users WHERE id=${req.query.id}`, (error, results, fields) => {
+            if (error) throw error;
+            res.status(200).json(results)
+        })
+        console.log(query.sql)
+    })
+})
 
 
 app.listen(5000, () => {
