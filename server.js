fadb29c07a732b3f905ddb617d40367a1755b215
carlojohn98 <johncarlomiel@gmail.com>
2019-03-14 20:23:52 +0800

    Deployment

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use("/public", express.static(path.join(__dirname + '/public')))
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 var server = app.listen(PORT, () => {
     console.log("Server started at port 8080")
 });
