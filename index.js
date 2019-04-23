9ab87c9a3b1adfadcea07ef0f11b28edb790e964
carlojohn98 <carlshit98@gmail.com>
2019-04-23 12:43:05 +0800

    hey

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ const bcrypt = require('bcrypt');
 
 const app = express();
 
+app.use(function (req, res, next) {
 
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', '*');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
+   // Pass to next layer of middleware
+   next();
+});
 
 
 
@@ -29,7 +46,7 @@ const PORT = process.env.PORT || 8080
 var server = app.listen(PORT, () => {
    console.log("Server started at port 8080")
 });
-var io = require('socket.io')(server, { origins: '*' }).listen(server);
+var io = require('socket.io').listen(server);
 
 
 //Routes
