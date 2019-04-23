8a21611094e99be532d8ffcb0383c0deea09b2ae
carlojohn98 <carlshit98@gmail.com>
2019-04-23 11:46:53 +0800

    hey

--- configs/config.js
+++ configs/config.js
@@ -1,15 +1,15 @@
 
 //For testing purposes
-module.exports = {
-    ip: "http://192.168.100.2:5000/",
-    secret_admin: "adminsecretshhhhhh",
-    secret_user: "shhhhhh"
-}
-
-
-// //For deployment configs
 // module.exports = {
-//     ip: "https://gordoncollegesds.herokuapp.com/",
+//     ip: "http://192.168.100.4:8080/",
 //     secret_admin: "adminsecretshhhhhh",
 //     secret_user: "shhhhhh"
 // }
+
+
+//For deployment configs
+module.exports = {
+    ip: "https://gordoncollegesds.herokuapp.com/",
+    secret_admin: "adminsecretshhhhhh",
+    secret_user: "shhhhhh"
+}
